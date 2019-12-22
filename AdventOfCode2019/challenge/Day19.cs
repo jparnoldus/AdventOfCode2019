004d051d1c0bcdc8c78098798d77b5ea3ee14003
Paul Arnoldus <jparnoldus@gmail.com>
2019-12-23 00:23:15 +0100

    Solved day 19 part 2

--- AdventOfCode2019/challenge/Day19.cs
+++ AdventOfCode2019/challenge/Day19.cs
@@ -141,15 +141,6 @@ namespace AdventOfCode2019.challenge
                 this.tile = tile;
             }
 
-            public List<Point> GetTopNeighbours()
-            {
-                return new List<Point>
-                {
-                    new Point(this.x, this.y - 1, '?'),
-                    new Point(this.x + 1, this.y, '?')
-                };
-            }
-
             public List<Point> GetBottomNeighbours()
             {
                 return new List<Point>
@@ -177,137 +168,95 @@ namespace AdventOfCode2019.challenge
 
         public static string Solve2()
         {
-            List<Point> map = new List<Point>();
-            List<Point> topEdges = new List<Point>();
-            List<Point> bottomEdges = new List<Point>();
-            List<Point> additions = new List<Point>();
-            bool notfound = true;
-            int max = 4020;
-            int minX = 4000;
-            int minY = 4000;
-            while (notfound)
+            int squareSize = 100 - 1;
+            bool found = false;
+            int y = 100;
+            int x = 0;
+            while (!found)
             {
-                additions.Clear();
-                for (int y = minY; y < max; y++)
+                while (Check(x, y) != 1)
                 {
-                    for (int x = minX; x < max; x++)
-                    {
-                        if (map.Contains(new Point(x, y, '.')))
-                            continue;
-
-                        List<long> commands = GetInputAsCsFloatListList(19).First();
-                        commands.AddRange(Enumerable.Repeat((long)0, 99999));
-
-                        bool hadX = false;
-                        long relativeBase = 0;
-                        for (long offset = 0; commands[(int)offset] != 99;)
-                        {
-                            string instruction = commands[(int)offset].ToString().PadLeft(5, '0');
-                            char firstMode = instruction[2];
-                            char secondMode = instruction[1];
-                            char thirdMode = instruction[0];
-                            int opcode = int.Parse(instruction[4].ToString());
-
-                            switch (opcode)
-                            {
-                                case 1:
-                                    if (thirdMode == '0')
-                                        commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) + GetParameter(commands, offset + 2, secondMode, relativeBase);
-                                    else if (thirdMode == '2')
-                                        commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) + GetParameter(commands, offset + 2, secondMode, relativeBase);
-                                    offset += 4;
-                                    break;
-                                case 2:
-                                    if (thirdMode == '0')
-                                        commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) * GetParameter(commands, offset + 2, secondMode, relativeBase);
-                                    else if (thirdMode == '2')
-                                        commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) * GetParameter(commands, offset + 2, secondMode, relativeBase);
-                                    offset += 4;
-                                    break;
-                                case 3:
-                                    long input = x;
-                                    if (hadX) { input = y; hadX = false; }
-                                    else hadX = true;
-                                    if (firstMode == '0')
-                                        commands[(int)commands[(int)(offset + 1)]] = input;
-                                    else if (firstMode == '2')
-                                        commands[(int)commands[(int)(offset + 1)] + (int)relativeBase] = input;
-                                    offset += 2;
-                                    break;
-                                case 4:
-                                    long output = GetParameter(commands, offset + 1, firstMode, relativeBase);
-                                    Point p = null;
-                                    if (output == 0)
-                                        p = new Point(x, y, '.');
-                                    else if (output == 1)
-                                        p = new Point(x, y, '#');
-                                    map.Add(p);
-                                    additions.Add(p);
-                                    offset += 2;
-                                    break;
-                                case 5:
-                                    offset = GetParameter(commands, offset + 1, firstMode, relativeBase) != 0 ? GetParameter(commands, offset + 2, secondMode, relativeBase) : offset + 3;
-                                    break;
-                                case 6:
-                                    offset = GetParameter(commands, offset + 1, firstMode, relativeBase) == 0 ? GetParameter(commands, offset + 2, secondMode, relativeBase) : offset + 3;
-                                    break;
-                                case 7:
-                                    if (thirdMode == '0')
-                                        commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) < GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
-                                    else if (thirdMode == '2')
-                                        commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) < GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
-                                    offset += 4;
-                                    break;
-                                case 8:
-                                    if (thirdMode == '0')
-                                        commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) == GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
-                                    else if (thirdMode == '2')
-                                        commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) == GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
-                                    offset += 4;
-                                    break;
-                                case 9:
-                                    relativeBase += GetParameter(commands, offset + 1, firstMode, relativeBase);
-                                    offset += 2;
-                                    break;
-                            }
-                        }
-                    }
+                    x++;
                 }
 
-                //check square
-                List<Point> newTopEdges = additions.Where(a => a.tile == '#' && a.GetTopNeighbours().Where(n => map.Exists(k => k.Equals(n))).Any(n => map.First(k => k.Equals(n)).tile == '.')).ToList();
-                List<Point> newBottomEdges = additions.Where(a => a.tile == '#' && a.GetBottomNeighbours().Where(n => map.Exists(k => k.Equals(n))).Any(n => map.First(k => k.Equals(n)).tile == '.')).ToList();
-                topEdges.AddRange(newTopEdges);
-
-                newBottomEdges.ForEach(a =>
-                {
-                    if (topEdges.Any(p => p.x == a.x + 100 && p.y == a.y - 100))
-                    {
-                        notfound = false;
-                    }
-                });
-
-                if (newTopEdges.Count > 0)
-                    minY = (int)newTopEdges.First().y - 5;
-                if (newBottomEdges.Count > 0)
-                    minX = (int)newBottomEdges.First().x - 5;
-                bottomEdges.AddRange(newBottomEdges);
-                max++;
+                found = Check(x + squareSize, y - squareSize) == 1;
+                y++;
             }
 
-            for (int y = 0; y < map.Max(t => t.y); y++)
+            return (x * 10000 + (y - squareSize - 1)).ToString();
+        }
+
+        private static long Check(int x, int y)
+        {
+            List<long> commands = GetInputAsCsFloatListList(19).First();
+            commands.AddRange(Enumerable.Repeat((long)0, 99999));
+
+            bool hadX = false;
+            long relativeBase = 0;
+            for (long offset = 0; commands[(int)offset] != 99;)
             {
-                for (int x = 0; x < map.Max(t => t.x); x++)
+                string instruction = commands[(int)offset].ToString().PadLeft(5, '0');
+                char firstMode = instruction[2];
+                char secondMode = instruction[1];
+                char thirdMode = instruction[0];
+                int opcode = int.Parse(instruction[4].ToString());
+
+                switch (opcode)
                 {
-                    if (map.Contains(new Point(x, y, '?')))
-                        Console.Write(map.First(t => t.Equals(new Point(x, y, '?'))).tile);
-                    else
-                        Console.Write("?");
+                    case 1:
+                        if (thirdMode == '0')
+                            commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) + GetParameter(commands, offset + 2, secondMode, relativeBase);
+                        else if (thirdMode == '2')
+                            commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) + GetParameter(commands, offset + 2, secondMode, relativeBase);
+                        offset += 4;
+                        break;
+                    case 2:
+                        if (thirdMode == '0')
+                            commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) * GetParameter(commands, offset + 2, secondMode, relativeBase);
+                        else if (thirdMode == '2')
+                            commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) * GetParameter(commands, offset + 2, secondMode, relativeBase);
+                        offset += 4;
+                        break;
+                    case 3:
+                        long input = x;
+                        if (hadX) { input = y; hadX = false; }
+                        else hadX = true;
+                        if (firstMode == '0')
+                            commands[(int)commands[(int)(offset + 1)]] = input;
+                        else if (firstMode == '2')
+                            commands[(int)commands[(int)(offset + 1)] + (int)relativeBase] = input;
+                        offset += 2;
+                        break;
+                    case 4:
+                        return GetParameter(commands, offset + 1, firstMode, relativeBase);
+                    case 5:
+                        offset = GetParameter(commands, offset + 1, firstMode, relativeBase) != 0 ? GetParameter(commands, offset + 2, secondMode, relativeBase) : offset + 3;
+                        break;
+                    case 6:
+                        offset = GetParameter(commands, offset + 1, firstMode, relativeBase) == 0 ? GetParameter(commands, offset + 2, secondMode, relativeBase) : offset + 3;
+                        break;
+                    case 7:
+                        if (thirdMode == '0')
+                            commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) < GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
+                        else if (thirdMode == '2')
+                            commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) < GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
+                        offset += 4;
+                        break;
+                    case 8:
+                        if (thirdMode == '0')
+                            commands[(int)commands[(int)(offset + 3)]] = GetParameter(commands, offset + 1, firstMode, relativeBase) == GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
+                        else if (thirdMode == '2')
+                            commands[(int)commands[(int)(offset + 3)] + (int)relativeBase] = GetParameter(commands, offset + 1, firstMode, relativeBase) == GetParameter(commands, offset + 2, secondMode, relativeBase) ? 1 : 0;
+                        offset += 4;
+                        break;
+                    case 9:
+                        relativeBase += GetParameter(commands, offset + 1, firstMode, relativeBase);
+                        offset += 2;
+                        break;
                 }
-                Console.WriteLine();
             }
 
-            return map.Count(p => p.tile == '#').ToString();
+            return 0;
         }
     }
 }
---
+++ AdventOfCode2019/challenge/Day22.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode2019.challenge
+{
+    class Day22 : Challenge
+    {
+        public static string Solve1()
+        {
+            int deckSize = 10007;
+            List<string> input = GetInputAsStringList(22);
+            List<int> deck = Enumerable.Range(0, deckSize).ToList();
+
+            foreach (string instruction in input)
+            {
+                if (instruction.StartsWith("cut -"))
+                {
+                    int skip = Math.Abs(int.Parse(instruction.Substring(4)));
+                    deck = deck.TakeLast(skip).Union(deck.SkipLast(skip)).ToList();
+                }
+                else if (instruction.StartsWith("cut"))
+                {
+                    int skip = int.Parse(instruction.Substring(4));
+                    deck = deck.Skip(skip).Union(deck.Take(skip)).ToList();
+                }
+                else if (instruction.StartsWith("deal into new stack"))
+                {
+                    deck.Reverse();
+                }
+                else if (instruction.StartsWith("deal with increment"))
+                {
+                    int increment = int.Parse(instruction.Substring("deal with increment ".Length));
+                    List<int> newDeck = Enumerable.Repeat(0, deckSize).ToList();
+                    int counter = 0;
+                    deck.ForEach(v => newDeck[counter++ * increment % deckSize] = v);
+                    deck = newDeck;
+                }
+            }
+
+            return deck.IndexOf(2020).ToString();
+        }
+
+        public static string Solve2()
+        {
+            return "";
+        }
+    }
+}
